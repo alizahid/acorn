83452cd11c3edb9e68bfcce5b1a7cfe82d3df83d
Ali Zahid <ali.zahid@live.com>
2024-08-02 17:16:16 +0500

    Image gallery updated

---
+++ apps/mobile/src/components/common/fake-modal.tsx
@@ -0,0 +1,103 @@
+import { type ReactNode, useEffect } from 'react'
+import { Modal, StyleSheet } from 'react-native'
+import { Gesture, GestureDetector } from 'react-native-gesture-handler'
+import Animated, {
+  interpolate,
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated'
+import { useSafeAreaFrame } from 'react-native-safe-area-context'
+import { createStyleSheet, useStyles } from 'react-native-unistyles'
+
+type Props = {
+  children: ReactNode
+  onClose: () => void
+  visible: boolean
+}
+
+export function FakeModal({ children, onClose, visible }: Props) {
+  const frame = useSafeAreaFrame()
+
+  const { styles } = useStyles(stylesheet)
+
+  const translate = useSharedValue(frame.height)
+
+  useEffect(() => {
+    if (visible) {
+      translate.value = withTiming(0, {
+        duration: 250,
+      })
+    }
+  }, [translate, visible])
+
+  const threshold = frame.height * 0.3
+
+  const gesture = Gesture.Pan()
+    .onUpdate((event) => {
+      translate.value = event.translationY > 0 ? event.translationY : 0
+    })
+    .onEnd((event) => {
+      if (event.translationY > threshold) {
+        translate.value = withTiming(
+          frame.height,
+          {
+            duration: 250,
+          },
+          () => {
+            runOnJS(onClose)()
+          },
+        )
+      } else {
+        translate.value = withTiming(0, {
+          duration: 250,
+        })
+      }
+    })
+
+  const overlayStyle = useAnimatedStyle(
+    () => ({
+      opacity: interpolate(translate.value, [0, frame.width], [1, 0]),
+    }),
+    [translate.value],
+  )
+
+  const contentStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateY: translate.value,
+      },
+    ],
+  }))
+
+  return (
+    <Modal transparent visible={visible}>
+      <Animated.View
+        pointerEvents="none"
+        style={[styles.overlay, overlayStyle]}
+      />
+
+      <GestureDetector gesture={gesture}>
+        <Animated.View style={[styles.main, contentStyle]}>
+          {children}
+        </Animated.View>
+      </GestureDetector>
+    </Modal>
+  )
+}
+
+const stylesheet = createStyleSheet((theme) => ({
+  header: (inset: number) => ({
+    position: 'absolute',
+    right: 0,
+    top: inset,
+  }),
+  main: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: theme.colors.gray[1],
+  },
+}))
--- apps/mobile/src/components/posts/gallery.tsx
+++ apps/mobile/src/components/posts/gallery.tsx
@@ -1,13 +1,8 @@
 import { Image } from 'expo-image'
 import { ImageZoom } from 'expo-image-zoom'
 import { useState } from 'react'
-import {
-  FlatList,
-  Modal,
-  type StyleProp,
-  View,
-  type ViewStyle,
-} from 'react-native'
+import { FlatList, type StyleProp, View, type ViewStyle } from 'react-native'
+import Animated from 'react-native-reanimated'
 import {
   useSafeAreaFrame,
   useSafeAreaInsets,
@@ -18,9 +13,11 @@ import { useTranslations } from 'use-intl'
 import { getDimensions } from '~/lib/media'
 import { type PostMedia } from '~/types/post'
 
+import { FakeModal } from '../common/fake-modal'
 import { Pressable } from '../common/pressable'
 import { Text } from '../common/text'
-import { HeaderButton } from '../navigation/header-button'
+
+const AnimatedImage = Animated.createAnimatedComponent(Image)
 
 type Props = {
   images: Array<PostMedia>
@@ -39,7 +36,6 @@ export function PostGalleryCard({ images, margin = 0, style }: Props) {
   const frameWidth = frame.width - margin
 
   const [visible, setVisible] = useState(false)
-  const [current, setCurrent] = useState(0)
 
   const first = getDimensions(frameWidth, images[0])
 
@@ -51,14 +47,14 @@ export function PostGalleryCard({ images, margin = 0, style }: Props) {
         }}
         style={style}
       >
-        <Image
-          source={images[current].url}
+        <AnimatedImage
+          source={images[0].url}
           style={[styles.image(first.height, first.width)]}
         />
 
         {images.length > 1 ? (
           <View style={styles.count}>
-            <Text size="1" tabular>
+            <Text contrast size="1" tabular>
               {t('items', {
                 count: images.length,
               })}
@@ -67,77 +63,42 @@ export function PostGalleryCard({ images, margin = 0, style }: Props) {
         ) : null}
       </Pressable>
 
-      <Modal
-        animationType="fade"
-        onRequestClose={() => {
+      <FakeModal
+        onClose={() => {
           setVisible(false)
         }}
-        style={styles.modal}
-        transparent
         visible={visible}
       >
         <FlatList
+          contentContainerStyle={styles.content(insets.top, insets.bottom)}
           data={images}
           decelerationRate="fast"
-          getItemLayout={(items, index) => ({
-            index,
-            length: frame.width,
-            offset: frame.width * index,
-          })}
           horizontal
-          initialScrollIndex={current}
           keyExtractor={(item) => item.url}
-          onScroll={(event) => {
-            const next = Math.round(
-              event.nativeEvent.contentOffset.x / frameWidth,
-            )
-
-            setCurrent(next)
-          }}
           renderItem={({ item }) => {
-            const dimensions = getDimensions(frame.width, item)
-
             return (
-              <Pressable
-                onPress={() => {
-                  setVisible(false)
-                }}
-                style={styles.item(dimensions.height, dimensions.width)}
-              >
-                <ImageZoom
-                  source={item.url}
-                  style={styles.image(dimensions.height, dimensions.width)}
-                />
-              </Pressable>
+              <ImageZoom
+                source={item.url}
+                style={styles.image(
+                  frame.height - insets.top - insets.bottom,
+                  frame.width,
+                )}
+              />
             )
           }}
           showsHorizontalScrollIndicator={false}
-          snapToOffsets={images.map((image, index) => frameWidth * index)}
-          style={styles.list(frame.height, frame.width)}
+          snapToOffsets={images.map((image, index) => frame.width * index)}
         />
-
-        <HeaderButton
-          icon="X"
-          onPress={() => {
-            setVisible(false)
-          }}
-          style={styles.header(insets.top)}
-        />
-
-        <View style={styles.footer(insets.bottom)}>
-          <Text size="1" tabular>
-            {t('item', {
-              count: images.length,
-              current: current + 1,
-            })}
-          </Text>
-        </View>
-      </Modal>
+      </FakeModal>
     </>
   )
 }
 
 const stylesheet = createStyleSheet((theme) => ({
+  content: (top: number, bottom: number) => ({
+    paddingBottom: bottom,
+    paddingTop: top,
+  }),
   count: {
     backgroundColor: theme.colors.black.a9,
     borderRadius: theme.radius[2],
@@ -147,37 +108,8 @@ const stylesheet = createStyleSheet((theme) => ({
     position: 'absolute',
     right: theme.space[2],
   },
-  footer: (inset: number) => ({
-    alignSelf: 'center',
-    backgroundColor: theme.colors.gray.a3,
-    borderRadius: theme.radius[2],
-    bottom: inset + theme.space[2],
-    paddingHorizontal: theme.space[1],
-    paddingVertical: theme.space[1] / 2,
-    position: 'absolute',
-  }),
-  header: (inset: number) => ({
-    position: 'absolute',
-    right: 0,
-    top: inset,
-  }),
   image: (height: number, width: number) => ({
-    backgroundColor: theme.colors.gray.a3,
     height,
     width,
   }),
-  item: (height: number, width: number) => ({
-    alignSelf: 'center',
-    backgroundColor: theme.colors.gray.a3,
-    height,
-    width,
-  }),
-  list: (height: number, width: number) => ({
-    backgroundColor: theme.colors.gray[1],
-    height,
-    width,
-  }),
-  modal: {
-    flex: 1,
-  },
 }))
