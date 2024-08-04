a0aef1a9c262333b9cdb08af232aaeeea053f284
Ali Zahid <ali.zahid@live.com>
2024-08-04 16:45:44 +0500

    Refactoring

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -1,12 +1,11 @@
 import 'expo-dev-client'
 import 'expo-router/entry'
-import 'react-native-reanimated'
 import 'fast-text-encoding'
-import '@formatjs/intl-getcanonicallocales/polyfill' // 1
-import '@formatjs/intl-locale/polyfill' // 2
-import '@formatjs/intl-pluralrules/polyfill' // 3
+import '@formatjs/intl-getcanonicallocales/polyfill-force' // 1
+import '@formatjs/intl-locale/polyfill-force' // 2
+import '@formatjs/intl-pluralrules/polyfill-force' // 3
 import '@formatjs/intl-pluralrules/locale-data/en' // 4
-import '@formatjs/intl-numberformat/polyfill' // 5
+import '@formatjs/intl-numberformat/polyfill-force' // 5
 import '@formatjs/intl-numberformat/locale-data/en' // 6
-import '@formatjs/intl-relativetimeformat/polyfill' // 7
+import '@formatjs/intl-relativetimeformat/polyfill-force' // 7
 import '@formatjs/intl-relativetimeformat/locale-data/en' // 8
