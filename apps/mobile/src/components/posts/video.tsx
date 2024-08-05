4465522a3ba57c1885fd7e7bfdc3a47456428da5
Ali Zahid <ali.zahid@live.com>
2024-08-05 21:48:38 +0500

    Video component updated

--- apps/mobile/src/components/posts/video.tsx
+++ apps/mobile/src/components/posts/video.tsx
@@ -1,11 +1,6 @@
-import { useVideoPlayer, type VideoContentFit, VideoView } from 'expo-video'
-import { useEffect, useRef, useState } from 'react'
-import { type StyleProp, View, type ViewStyle } from 'react-native'
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-  withTiming,
-} from 'react-native-reanimated'
+import { useVideoPlayer, VideoView } from 'expo-video'
+import { useEffect, useState } from 'react'
+import { type StyleProp, type ViewStyle } from 'react-native'
 import { createStyleSheet, useStyles } from 'react-native-unistyles'
 
 import { useCommon } from '~/hooks/common'
@@ -13,6 +8,7 @@ import { getDimensions } from '~/lib/media'
 import { usePreferences } from '~/stores/preferences'
 import { type PostMedia } from '~/types/post'
 
+import { FakeModal } from '../common/fake-modal'
 import { Icon } from '../common/icon'
 import { Pressable } from '../common/pressable'
 
@@ -26,16 +22,11 @@ type Props = {
 export function PostVideoCard({ margin = 0, style, video, viewing }: Props) {
   const common = useCommon()
 
-  const ref = useRef<VideoView>(null)
-
   const { muted, updatePreferences } = usePreferences()
 
   const { styles, theme } = useStyles(stylesheet)
 
-  const opacity = useSharedValue(0)
-
-  const [playing, setPlaying] = useState(false)
-  const [contentFit, setContentFit] = useState<VideoContentFit>('cover')
+  const [visible, setVisible] = useState(false)
 
   const frameWidth = common.frame.width - margin
   const maxHeight =
@@ -54,144 +45,86 @@ export function PostVideoCard({ margin = 0, style, video, viewing }: Props) {
   })
 
   useEffect(() => {
-    const subscription = player.addListener('playingChange', (isPlaying) => {
-      setPlaying(isPlaying)
-    })
-
-    return () => {
-      subscription.remove()
-    }
-  }, [player])
-
-  useEffect(() => {
-    player.muted = !viewing || muted
+    player.muted = visible ? false : !viewing || muted
 
     if (viewing) {
       player.play()
     } else {
       player.pause()
     }
-  }, [muted, player, viewing])
-
-  const controls = [
-    {
-      icon: 'Resize',
-      key: 'resize',
-      onPress() {
-        setContentFit((previous) =>
-          previous === 'contain' ? 'cover' : 'contain',
-        )
-      },
-      weight: 'regular',
-    },
-    {
-      icon: 'Rewind',
-      key: 'rewind',
-      onPress() {
-        player.seekBy(-10)
-      },
-      weight: 'fill',
-    },
-    {
-      icon: playing ? 'Pause' : 'Play',
-      key: 'pause',
-      onPress() {
-        if (playing) {
-          player.pause()
-        } else {
-          player.play()
-        }
-      },
-      weight: 'fill',
-    },
-    {
-      icon: 'FastForward',
-      key: 'forward',
-      onPress() {
-        player.seekBy(10)
-      },
-      weight: 'fill',
-    },
-    {
-      icon: muted ? 'SpeakerSimpleX' : 'SpeakerSimpleHigh',
-      key: 'volume',
-      onPress() {
-        updatePreferences({
-          muted: !muted,
-        })
-      },
-      weight: 'regular',
-    },
-  ] as const
+  }, [muted, player, viewing, visible])
 
   const dimensions = getDimensions(frameWidth, video)
 
-  const controlsStyle = useAnimatedStyle(() => ({
-    opacity: opacity.value,
-  }))
-
   return (
-    <View style={[styles.main, style]}>
+    <>
       <Pressable
         onPress={() => {
-          opacity.value = withTiming(opacity.value === 1 ? 0 : 1, {
-            duration: 250,
-          })
+          setVisible(true)
         }}
-        without
+        style={style}
       >
         <VideoView
           allowsFullscreen={false}
           allowsPictureInPicture={false}
           allowsVideoFrameAnalysis={false}
-          contentFit={contentFit}
+          contentFit="cover"
           nativeControls={false}
           player={player}
-          ref={ref}
-          style={styles.video(maxHeight, dimensions.height, dimensions.width)}
+          style={styles.main(maxHeight, dimensions.height, dimensions.width)}
         />
+
+        <Pressable
+          hitSlop={theme.space[3]}
+          onPress={() => {
+            updatePreferences({
+              muted: !muted,
+            })
+          }}
+          style={styles.volume}
+        >
+          <Icon
+            color={theme.colors.white.a11}
+            name={muted ? 'SpeakerSimpleX' : 'SpeakerSimpleHigh'}
+            size={theme.space[4]}
+          />
+        </Pressable>
       </Pressable>
 
-      <Animated.View style={[styles.controls, controlsStyle]}>
-        {controls.map((control) => (
-          <Pressable
-            key={control.key}
-            onPress={control.onPress}
-            style={styles.control}
-          >
-            <Icon
-              color={theme.colors.white.a11}
-              name={control.icon}
-              size={theme.space[5]}
-              weight={control.weight}
-            />
-          </Pressable>
-        ))}
-      </Animated.View>
-    </View>
+      <FakeModal
+        onClose={() => {
+          setVisible(false)
+        }}
+        visible={visible}
+      >
+        <VideoView
+          allowsFullscreen={false}
+          allowsPictureInPicture={false}
+          allowsVideoFrameAnalysis={false}
+          contentFit="contain"
+          nativeControls
+          player={player}
+          style={styles.video}
+        />
+      </FakeModal>
+    </>
   )
 }
 
 const stylesheet = createStyleSheet((theme) => ({
-  control: {
-    backgroundColor: theme.colors.black.a9,
-    borderRadius: theme.space[5],
-    padding: theme.space[3],
-  },
-  controls: {
-    bottom: theme.space[4],
-    flexDirection: 'row',
-    gap: theme.space[2],
-    justifyContent: 'center',
-    left: theme.space[4],
-    position: 'absolute',
-    right: theme.space[4],
-  },
-  main: {
-    backgroundColor: theme.colors.gray.a2,
-  },
-  video: (maxHeight: number, height: number, width: number) => ({
+  main: (maxHeight: number, height: number, width: number) => ({
     height: Math.min(maxHeight, height),
     width,
   }),
+  video: {
+    flex: 1,
+  },
+  volume: {
+    backgroundColor: theme.colors.black.a9,
+    borderRadius: theme.space[4],
+    bottom: theme.space[3],
+    padding: theme.space[2],
+    position: 'absolute',
+    right: theme.space[3],
+  },
 }))
