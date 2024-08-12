897c00b2ccc729a4dae05d694758c073cbff9ff2
Ali Zahid <ali.zahid@live.com>
2024-08-12 08:35:14 +0500

    Initial commit

---
+++ packages/eslint/.eslintrc.js
@@ -0,0 +1,5 @@
+const { create } = require('.')
+
+module.exports = create({
+  root: true,
+})
---
+++ packages/eslint/index.js
@@ -0,0 +1,121 @@
+const { resolve } = require('node:path')
+
+/**
+ * @param {{
+ *   ignorePatterns?: string | string[]
+ *   next?: boolean
+ *   overrides?: import('eslint').Linter.ConfigOverride[]
+ *   root?: boolean
+ * }}
+ *
+ * @returns {import('eslint').Linter.Config}
+ */
+exports.create = ({ ignorePatterns = [], next, overrides = [], root }) => {
+  const project = resolve(process.cwd(), 'tsconfig.json')
+
+  /** @type {import('eslint').Linter.Config} */
+  const config = {
+    extends: [
+      'eslint:recommended',
+      require.resolve('@vercel/style-guide/eslint/node'),
+      require.resolve('@vercel/style-guide/eslint/browser'),
+      require.resolve('@vercel/style-guide/eslint/typescript'),
+      require.resolve('@vercel/style-guide/eslint/react'),
+      next && require.resolve('@vercel/style-guide/eslint/next'),
+      'turbo',
+      'plugin:prettier/recommended',
+    ].filter(Boolean),
+    globals: {
+      JSX: true,
+      React: true,
+    },
+    ignorePatterns: [...ignorePatterns, 'node_modules/', 'dist/', 'build/'],
+    overrides: [
+      ...overrides,
+      {
+        files: ['*.ts', '*.tsx', '*.d.ts'],
+        parserOptions: {
+          project,
+        },
+        rules: {
+          '@typescript-eslint/array-type': [
+            'error',
+            {
+              default: 'generic',
+            },
+          ],
+          '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+          '@typescript-eslint/consistent-type-imports': [
+            'warn',
+            {
+              fixStyle: 'inline-type-imports',
+            },
+          ],
+          '@typescript-eslint/explicit-function-return-type': 'off',
+          '@typescript-eslint/no-misused-promises': [
+            'error',
+            {
+              checksVoidReturn: {
+                attributes: false,
+              },
+            },
+          ],
+          '@typescript-eslint/no-non-null-assertion': 'off',
+          '@typescript-eslint/restrict-template-expressions': [
+            'error',
+            {
+              allowAny: false,
+              allowBoolean: false,
+              allowNever: false,
+              allowNullish: false,
+              allowNumber: true,
+              allowRegExp: false,
+            },
+          ],
+          'typescript-sort-keys/interface': 'error',
+        },
+        settings: {
+          'import/resolver': {
+            typescript: {
+              project,
+            },
+          },
+        },
+      },
+      {
+        files: ['*.tsx'],
+        rules: {
+          'jsx-a11y/label-has-associated-control': 'off',
+          'react/jsx-sort-props': 'error',
+          'react/no-unstable-nested-components': 'off',
+        },
+      },
+    ],
+    plugins: [
+      'sort-keys-fix',
+      'sort-destructure-keys',
+      'simple-import-sort',
+      'typescript-sort-keys',
+    ],
+    root,
+    rules: {
+      'import/no-default-export': 'off',
+      'import/no-extraneous-dependencies': [
+        'error',
+        {
+          includeInternal: false,
+        },
+      ],
+      'import/order': 'off',
+      'no-nested-ternary': 'off',
+      'prefer-named-capture-group': 'off',
+      'prettier/prettier': 'error',
+      'simple-import-sort/exports': 'error',
+      'simple-import-sort/imports': 'error',
+      'sort-destructure-keys/sort-destructure-keys': 'error',
+      'sort-keys-fix/sort-keys-fix': 'error',
+    },
+  }
+
+  return config
+}
