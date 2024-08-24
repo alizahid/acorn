09858c309be3eda6de06709467e0c68b8c1f30e9
Ali Zahid <ali.zahid@live.com>
2024-08-24 14:45:31 +0500

    Query focus manager added

--- apps/mobile/src/components/navigation/root.tsx
+++ apps/mobile/src/components/navigation/root.tsx
@@ -1,4 +1,7 @@
+import { focusManager } from '@tanstack/react-query'
 import { Stack, useFocusEffect, useRouter } from 'expo-router'
+import { useEffect } from 'react'
+import { AppState } from 'react-native'
 
 import { useAuth } from '~/stores/auth'
 
@@ -14,6 +17,16 @@ export function Root() {
       router.navigate('/sign-in')
     }
   })
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', (status) => {
+      focusManager.setFocused(status === 'active')
+    })
+
+    return () => {
+      subscription.remove()
+    }
+  }, [])
 
   return (
     <Stack
