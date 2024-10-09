424e365c3d078585826f3edb2d66af8425f2e0a2
Ali Zahid <ali.zahid@live.com>
2024-10-09 20:12:53 +0500

    Bug fix

--- apps/mobile/src/components/navigation/screens/tabs/search.tsx
+++ apps/mobile/src/components/navigation/screens/tabs/search.tsx
@@ -30,7 +30,7 @@ export function SearchScreen() {
     sort: 'relevance',
   })
 
-  const [debounced] = useDebounce(query, 500)
+  const [debounced] = useDebounce(query, 1_000)
 
   return (
     <Tabs.Container
