3e3a23988a4452456cf1762e95aed801c1f14c55
Ali Zahid <ali.zahid@live.com>
2024-10-30 05:12:26 +0500

    Version bumped

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -67,7 +67,7 @@ export default function getConfig({ config }: ConfigContext): ExpoConfig {
       },
     },
     ios: {
-      buildNumber: '21',
+      buildNumber: '22',
       bundleIdentifier,
       config: {
         usesNonExemptEncryption: false,
