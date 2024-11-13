cedfd9298ec86485236e14eda44c3feb45b4113f
Ali Zahid <ali.zahid@live.com>
2024-11-13 22:07:28 +0500

    Bug fix

--- apps/mobile/src/sheets/feed-type.tsx
+++ apps/mobile/src/sheets/feed-type.tsx
@@ -1,18 +1,18 @@
-import BottomSheet, { BottomSheetSectionList } from '@gorhom/bottom-sheet'
+import BottomSheet, { BottomSheetFlashList } from '@gorhom/bottom-sheet'
 import { Image } from 'expo-image'
+import { compact } from 'lodash'
 import { useRef } from 'react'
 import { createCallable } from 'react-call'
 import { createStyleSheet, useStyles } from 'react-native-unistyles'
 import { useTranslations } from 'use-intl'
 
 import { Icon } from '~/components/common/icon'
+import { View } from '~/components/common/view'
 import { useCommunities } from '~/hooks/queries/communities/communities'
 import { useFeeds } from '~/hooks/queries/feeds/feeds'
 import { listProps } from '~/lib/common'
 import { removePrefix } from '~/lib/reddit'
 import { FeedTypeColors, FeedTypeIcons } from '~/lib/sort'
-import { type Community } from '~/types/community'
-import { type Feed } from '~/types/feed'
 import { FeedType } from '~/types/sort'
 
 import { Spinner } from '../components/common/spinner'
@@ -29,18 +29,10 @@ export type FeedTypeSheetProps = {
 
 export type FeedTypeSheetReturn = FeedTypeSheetProps
 
-type Item = FeedType | Feed | Community
-
-type Section = {
-  loading?: boolean
-  title: string
-  type: 'type' | 'feed' | 'community' | 'user'
-}
-
 export const FeedTypeSheet = createCallable<
   FeedTypeSheetProps,
   FeedTypeSheetReturn
->(function Component({ call, community, feed, type }) {
+>(function Component({ call, community, feed, type, user }) {
   const t = useTranslations('component.common.type')
 
   const { styles, theme } = useStyles(stylesheet)
@@ -49,6 +41,96 @@ export const FeedTypeSheet = createCallable<
 
   const { feeds, isLoading: loadingFeeds } = useFeeds()
   const { communities, isLoading: loadingCommunities, users } = useCommunities()
+
+  const data = compact([
+    {
+      key: 'type-title',
+      title: t('type.title'),
+      type: 'header' as const,
+    },
+    ...FeedType.map((item) => ({
+      item,
+      key: item,
+      type: 'type' as const,
+    })),
+
+    {
+      key: 'feed-separator',
+      type: 'separator' as const,
+    },
+    {
+      key: 'feed',
+      title: t('feeds.title'),
+      type: 'header' as const,
+    },
+    loadingFeeds && {
+      key: 'feed-spinner',
+      type: 'spinner' as const,
+    },
+    ...feeds.map((item) => ({
+      item,
+      key: item.id,
+      type: 'feed' as const,
+    })),
+    feeds.length === 0 && {
+      key: 'feed-empty',
+      type: 'empty' as const,
+    },
+
+    {
+      key: 'community-separator',
+      type: 'separator' as const,
+    },
+    {
+      key: 'community',
+      title: t('communities.title'),
+      type: 'header' as const,
+    },
+    loadingCommunities && {
+      key: 'community-spinner',
+      type: 'spinner' as const,
+    },
+    ...communities
+      .filter((item) => typeof item !== 'string')
+      .map((item) => ({
+        item,
+        key: item.id,
+        type: 'community' as const,
+      })),
+    communities.length === 0 && {
+      key: 'community-empty',
+      type: 'empty' as const,
+    },
+
+    {
+      key: 'user-separator',
+      type: 'separator' as const,
+    },
+    {
+      key: 'user',
+      title: t('users.title'),
+      type: 'header' as const,
+    },
+    loadingCommunities && {
+      key: 'user-spinner',
+      type: 'spinner' as const,
+    },
+    ...users
+      .filter((item) => typeof item !== 'string')
+      .map((item) => ({
+        item,
+        key: item.id,
+        type: 'user' as const,
+      })),
+    users.length === 0 && {
+      key: 'user-empty',
+      type: 'empty' as const,
+    },
+  ])
+
+  const sticky = data
+    .map((item, index) => (item.type === 'header' ? index : null))
+    .filter((item) => item !== null) as unknown as Array<number>
 
   return (
     <BottomSheet
@@ -60,67 +142,19 @@ export const FeedTypeSheet = createCallable<
       ref={sheet}
       style={styles.main}
     >
-      <BottomSheetSectionList<Item, Section>
+      <BottomSheetFlashList
         {...listProps}
         contentContainerStyle={styles.content}
-        renderItem={({ item, section }) => {
-          if (typeof item === 'string') {
-            return (
-              <SheetItem
-                icon={{
-                  color: theme.colors[FeedTypeColors[item]].a9,
-                  name: FeedTypeIcons[item],
-                }}
-                key={item}
-                label={t(`type.${item}`)}
-                onPress={() => {
-                  sheet.current?.close()
-
-                  call.end({
-                    type: item,
-                  })
-                }}
-                selected={!feed && !community && item === type}
-              />
-            )
+        data={data}
+        estimatedItemSize={48}
+        getItemType={(item) => item.type}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => {
+          if (item.type === 'separator') {
+            return <View height="4" />
           }
 
-          return (
-            <SheetItem
-              key={item.id}
-              label={item.name}
-              left={<Image source={item.image} style={styles.image} />}
-              onPress={() => {
-                sheet.current?.close()
-
-                call.end({
-                  [section.type]:
-                    section.type === 'feed'
-                      ? item.id
-                      : section.type === 'user'
-                        ? removePrefix(item.name)
-                        : item.name,
-                })
-              }}
-              right={
-                'favorite' in item && item.favorite ? (
-                  <Icon
-                    color={theme.colors.amber.a9}
-                    name="Star"
-                    weight="fill"
-                  />
-                ) : null
-              }
-              selected={item.name === community}
-            />
-          )
-        }}
-        renderSectionFooter={({ section }) => {
-          if (section.loading) {
-            return <Spinner m="4" />
-          }
-
-          if (section.data.length === 0) {
+          if (item.type === 'empty') {
             return (
               <Icon
                 color={theme.colors.accent.a9}
@@ -132,36 +166,70 @@ export const FeedTypeSheet = createCallable<
             )
           }
 
-          return null
+          if (item.type === 'spinner') {
+            return <Spinner m="4" />
+          }
+
+          if (item.type === 'header') {
+            return <SheetHeader title={item.title} />
+          }
+
+          if (item.type === 'type') {
+            return (
+              <SheetItem
+                icon={{
+                  color: theme.colors[FeedTypeColors[item.item]].a9,
+                  name: FeedTypeIcons[item.item],
+                }}
+                label={t(`type.${item.item}`)}
+                onPress={() => {
+                  sheet.current?.close()
+
+                  call.end({
+                    type: item.item,
+                  })
+                }}
+                selected={!feed && !community && item.item === type}
+              />
+            )
+          }
+
+          return (
+            <SheetItem
+              label={item.item.name}
+              left={<Image source={item.item.image} style={styles.image} />}
+              onPress={() => {
+                sheet.current?.close()
+
+                call.end({
+                  [item.type]:
+                    item.type === 'feed'
+                      ? item.item.id
+                      : item.type === 'user'
+                        ? removePrefix(item.item.name)
+                        : item.item.name,
+                })
+              }}
+              right={
+                'favorite' in item && item.favorite ? (
+                  <Icon
+                    color={theme.colors.amber.a9}
+                    name="Star"
+                    weight="fill"
+                  />
+                ) : null
+              }
+              selected={
+                item.type === 'feed'
+                  ? item.item.id === feed
+                  : item.type === 'community'
+                    ? item.item.name === community
+                    : item.item.name === user
+              }
+            />
+          )
         }}
-        renderSectionHeader={({ section }) => (
-          <SheetHeader style={styles.header} title={section.title} />
-        )}
-        sections={[
-          {
-            data: FeedType,
-            title: t('type.title'),
-            type: 'type',
-          },
-          {
-            data: feeds,
-            loading: loadingFeeds,
-            title: t('feeds.title'),
-            type: 'feed',
-          },
-          {
-            data: communities.filter((item) => typeof item !== 'string'),
-            loading: loadingCommunities,
-            title: t('communities.title'),
-            type: 'community',
-          },
-          {
-            data: users.filter((item) => typeof item !== 'string'),
-            loading: loadingCommunities,
-            title: t('users.title'),
-            type: 'user',
-          },
-        ]}
+        stickyHeaderIndices={sticky}
       />
     </BottomSheet>
   )
