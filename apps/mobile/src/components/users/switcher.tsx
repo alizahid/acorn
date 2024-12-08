6c956a7d0ce585210f2586030f60d3fc1862dd78
Ali Zahid <ali.zahid@live.com>
2024-12-08 17:05:27 +0400

    Design updated

--- apps/mobile/src/components/users/switcher.tsx
+++ apps/mobile/src/components/users/switcher.tsx
@@ -1,19 +1,28 @@
 import { useRouter } from 'expo-router'
-import { useState } from 'react'
-import { Modal, StyleSheet } from 'react-native'
+import { useCallback, useState } from 'react'
+import { Modal, Pressable, StyleSheet } from 'react-native'
 import Swipeable from 'react-native-gesture-handler/ReanimatedSwipeable'
+import Animated, {
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated'
+import { useSafeAreaFrame } from 'react-native-safe-area-context'
 import { createStyleSheet, useStyles } from 'react-native-unistyles'
 import { useTranslations } from 'use-intl'
 
 import { useAuth } from '~/stores/auth'
 
-import { Pressable } from '../common/pressable'
 import { Text } from '../common/text'
-import { View } from '../common/view'
 import { HeaderButton } from '../navigation/header-button'
 import { SheetHeader } from '../sheets/header'
 
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable)
+
 export function UserSwitcher() {
+  const frame = useSafeAreaFrame()
+
   const router = useRouter()
 
   const t = useTranslations('component.users.switcher')
@@ -22,96 +31,118 @@ export function UserSwitcher() {
 
   const { styles } = useStyles(stylesheet)
 
+  const opacity = useSharedValue(0)
+  const translate = useSharedValue(frame.height)
+
   const [open, setOpen] = useState(false)
+
+  const onOpen = useCallback(() => {
+    setOpen(true)
+
+    opacity.set(() => withTiming(1))
+    translate.set(() => withTiming(0))
+  }, [opacity, translate])
+
+  const onClose = useCallback(() => {
+    opacity.set(() => withTiming(0))
+
+    translate.set(() =>
+      withTiming(frame.height, undefined, () => {
+        runOnJS(setOpen)(false)
+      }),
+    )
+  }, [frame.height, opacity, translate])
+
+  const main = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateY: translate.get(),
+      },
+    ],
+  }))
+
+  const overlay = useAnimatedStyle(() => ({
+    opacity: opacity.get(),
+  }))
 
   return (
     <>
       <HeaderButton
         icon="UserSwitch"
         onPress={() => {
-          setOpen(true)
+          onOpen()
         }}
       />
 
-      <Modal animationType="fade" transparent visible={open}>
-        <View align="center" justify="center" style={styles.main}>
-          <Pressable
-            onPress={() => {
-              setOpen(false)
-            }}
-            style={styles.overlay}
+      <Modal transparent visible={open}>
+        <AnimatedPressable
+          onPress={() => {
+            onClose()
+          }}
+          style={[styles.overlay, overlay]}
+        />
+
+        <Animated.View style={[styles.main, main]}>
+          <SheetHeader
+            right={
+              <HeaderButton
+                color="green"
+                icon="PlusCircle"
+                onPress={() => {
+                  router.navigate({
+                    params: {
+                      mode: 'dismissible',
+                    },
+                    pathname: '/sign-in',
+                  })
+
+                  onClose()
+                }}
+              />
+            }
+            style={styles.header}
+            title={t('title')}
           />
 
-          <View style={styles.content}>
-            <SheetHeader
-              right={
+          {accounts.map((account) => (
+            <Swipeable
+              containerStyle={styles.delete}
+              key={account.id}
+              renderLeftActions={() => (
                 <HeaderButton
-                  color="green"
-                  icon="PlusCircle"
+                  contrast
+                  icon="Trash"
                   onPress={() => {
-                    router.navigate({
-                      params: {
-                        mode: 'dismissible',
-                      },
-                      pathname: '/sign-in',
-                    })
-
-                    setOpen(false)
+                    removeAccount(account.id)
                   }}
                 />
-              }
-              style={styles.header}
-              title={t('title')}
-            />
+              )}
+              renderRightActions={() => null}
+            >
+              <Pressable
+                onPress={() => {
+                  if (account.id !== accountId) {
+                    setAccount(account.id)
+                  }
 
-            {accounts.map((account) => (
-              <Swipeable
-                containerStyle={styles.delete}
-                key={account.id}
-                renderLeftActions={() => null}
-                renderRightActions={() => (
-                  <HeaderButton
-                    contrast
-                    icon="Trash"
-                    onPress={() => {
-                      removeAccount(account.id)
-                    }}
-                  />
-                )}
+                  onClose()
+                }}
+                style={[
+                  styles.item,
+                  account.id === accountId && styles.selected,
+                ]}
               >
-                <Pressable
-                  onPress={() => {
-                    if (account.id !== accountId) {
-                      setAccount(account.id)
-                    }
-
-                    setOpen(false)
-                  }}
-                  p="3"
-                  style={[
-                    styles.item,
-                    account.id === accountId && styles.selected,
-                  ]}
-                >
-                  <Text weight="medium">{account.id}</Text>
-                </Pressable>
-              </Swipeable>
-            ))}
-          </View>
-        </View>
+                <Text weight="medium">{account.id}</Text>
+              </Pressable>
+            </Swipeable>
+          ))}
+        </Animated.View>
       </Modal>
     </>
   )
 }
 
 const stylesheet = createStyleSheet((theme, runtime) => ({
-  content: {
-    backgroundColor: theme.colors.gray[1],
-    borderCurve: 'continuous',
-    borderRadius: theme.radius[5],
-    overflow: 'hidden',
-    width: runtime.screen.width - theme.space[6],
-  },
   delete: {
     backgroundColor: theme.colors.red.a9,
   },
@@ -120,13 +151,24 @@ const stylesheet = createStyleSheet((theme, runtime) => ({
   },
   item: {
     backgroundColor: theme.colors.gray[1],
+    padding: theme.space[3],
   },
   main: {
-    ...StyleSheet.absoluteFillObject,
+    backgroundColor: theme.colors.gray[1],
+    borderCurve: 'continuous',
+    borderTopLeftRadius: theme.radius[5],
+    borderTopRightRadius: theme.radius[5],
+    bottom: 0,
+    left: 0,
+    overflow: 'hidden',
+    paddingBottom: runtime.insets.bottom,
+    position: 'absolute',
+    right: 0,
+    width: runtime.screen.width,
   },
   overlay: {
     ...StyleSheet.absoluteFillObject,
-    backgroundColor: theme.colors.gray.a6,
+    backgroundColor: theme.colors.gray.a7,
   },
   selected: {
     backgroundColor: theme.colors.accent[5],
