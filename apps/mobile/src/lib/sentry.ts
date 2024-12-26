f6fe7ec7983ea657645584aee4518b571e3b6a66
Ali Zahid <ali.zahid@live.com>
2024-12-26 16:40:06 +0400

    Disable Sentry in dev

--- apps/mobile/src/lib/sentry.ts
+++ apps/mobile/src/lib/sentry.ts
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/react-native'
 
 Sentry.init({
   dsn: String(process.env.EXPO_PUBLIC_SENTRY_DSN),
-  enabled: Boolean(process.env.EXPO_PUBLIC_SENTRY_DSN),
+  enabled: !__DEV__ && Boolean(process.env.EXPO_PUBLIC_SENTRY_DSN),
   tracesSampleRate: 0.01,
 })
 
