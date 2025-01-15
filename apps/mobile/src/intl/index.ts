b43ca00f7e60ef7d8c8b68dea9018e05356d89d3
Ali Zahid <ali.zahid@live.com>
2025-01-16 01:37:54 +0500

    Refactoring

--- apps/mobile/src/intl/index.ts
+++ apps/mobile/src/intl/index.ts
@@ -1,12 +1,4 @@
 import { getCalendars } from 'expo-localization'
-import {
-  createFormatter,
-  createTranslator,
-  type NamespaceKeys,
-  type NestedKeyOf,
-} from 'use-intl'
-
-import messages from './en.json'
 
 const [calendar] = getCalendars()
 
@@ -14,23 +6,3 @@ export const timeZone =
   (calendar?.timeZone === 'Asia/Kolkata'
     ? 'Asia/Calcutta'
     : calendar?.timeZone) ?? undefined
-
-export function getTranslator(
-  namespace?: NamespaceKeys<IntlMessages, NestedKeyOf<IntlMessages>>,
-) {
-  return createTranslator({
-    locale: 'en',
-    messages,
-    namespace,
-    now: new Date(),
-    timeZone,
-  })
-}
-
-export function getFormatter() {
-  return createFormatter({
-    locale: 'en',
-    now: new Date(),
-    timeZone,
-  })
-}
