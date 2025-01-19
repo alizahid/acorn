309b1bcc64ea77c4cb0e2988356ea5c50068d265
Ali Zahid <ali.zahid@live.com>
2025-01-19 23:50:23 +0500

    Version bumped

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -76,7 +76,7 @@ export default function getConfig(context: ConfigContext): ExpoConfig {
     },
     ios: {
       appleTeamId: String(process.env.APPLE_TEAM_ID),
-      buildNumber: '38',
+      buildNumber: '39',
       bundleIdentifier,
       config: {
         usesNonExemptEncryption: false,
