d3cface342f2613838b24bb5874d99a9c3ad4bc8
Ali Zahid <ali.zahid@live.com>
2025-01-20 05:05:35 +0500

    Allow extension on older iOS versions

--- apps/mobile/targets/safari/expo-target.config.js
+++ apps/mobile/targets/safari/expo-target.config.js
@@ -1,5 +1,6 @@
 /** @type {import('@bacons/apple-targets/app.plugin').ConfigFunction} */
 module.exports = () => ({
+  deploymentTarget: '15.1',
   entitlements: {},
   icon: 'https://acorn.blue/images/acorn.png',
   type: 'safari',
