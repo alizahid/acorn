374adcbf8bd1520eb4e76159e9fb0f611b00d2ff
Ali Zahid <ali.zahid@live.com>
2025-02-18 10:23:37 +0500

    Styles updated

--- apps/mobile/src/components/common/slider.tsx
+++ apps/mobile/src/components/common/slider.tsx
@@ -1,6 +1,9 @@
 import Component from '@react-native-community/slider'
+import { range } from 'lodash'
 import { type StyleProp, type ViewStyle } from 'react-native'
-import { useStyles } from 'react-native-unistyles'
+import { createStyleSheet, useStyles } from 'react-native-unistyles'
+
+import { View } from './view'
 
 type Props = {
   disabled?: boolean
@@ -21,19 +24,53 @@ export function Slider({
   style,
   value,
 }: Props) {
-  const { theme } = useStyles()
+  const { styles, theme } = useStyles(stylesheet)
 
   return (
-    <Component
-      disabled={disabled}
-      maximumTrackTintColor={theme.colors.gray.ui}
-      maximumValue={max}
-      minimumTrackTintColor={theme.colors.accent.accent}
-      minimumValue={min}
-      onValueChange={onChange}
-      step={step}
-      style={style}
-      value={value}
-    />
+    <View style={style}>
+      {!disabled && step && min && max ? (
+        <View
+          align="center"
+          direction="row"
+          justify="between"
+          pointerEvents="none"
+          style={styles.markers}
+        >
+          {range(min, max + step, step).map((item) => (
+            <View key={item} style={styles.marker(item <= value)} />
+          ))}
+        </View>
+      ) : null}
+
+      <Component
+        disabled={disabled}
+        maximumTrackTintColor={theme.colors.gray.ui}
+        maximumValue={max}
+        minimumTrackTintColor={theme.colors.accent.accent}
+        minimumValue={min}
+        onValueChange={onChange}
+        step={step}
+        tapToSeek
+        value={value}
+      />
+    </View>
   )
 }
+
+const stylesheet = createStyleSheet((theme) => ({
+  marker: (enabled: boolean) => ({
+    backgroundColor: enabled
+      ? theme.colors.accent.accent
+      : theme.colors.gray.ui,
+    borderCurve: 'continuous',
+    borderRadius: theme.space[4],
+    height: theme.space[3],
+    width: theme.space[1],
+  }),
+  markers: {
+    height: '100%',
+    left: 14,
+    position: 'absolute',
+    right: 14,
+  },
+}))
