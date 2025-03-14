3b213ae25647deade2de451bdbde2ac75b346ea3
Ali Zahid <ali.zahid@live.com>
2025-03-14 10:35:28 +0500

    Dependencies upgraded

--- apps/mobile/typings/intl.d.ts
+++ apps/mobile/typings/intl.d.ts
@@ -1,8 +1,9 @@
 import type en from '~/intl/en.json'
 
-type Messages = typeof en
-
-declare global {
-  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions, @typescript-eslint/no-empty-interface -- go away
-  interface IntlMessages extends Messages {}
+declare module 'use-intl' {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions -- go away
+  interface AppConfig {
+    Locale: 'en'
+    Messages: typeof en
+  }
 }
