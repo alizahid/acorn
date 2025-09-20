acd168069324832c9849a06db8fdd8fed48a2d19
Ali Zahid <ali.zahid@live.com>
2025-09-21 01:29:41 +0500

    Refactoring

--- apps/mobile/src/components/common/icon/index.tsx
+++ apps/mobile/src/components/common/icon/index.tsx
@@ -4,4 +4,5 @@ import { withUnistyles } from 'react-native-unistyles'
 export const Icon = withUnistyles(SymbolView, (theme) => ({
   size: theme.space[5],
   tintColor: theme.colors.accent.accent,
+  type: 'hierarchical' as const,
 }))
