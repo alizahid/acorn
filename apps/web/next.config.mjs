36d8c94b9307ad3811e88a61703f96d567c90960
Ali Zahid <ali.zahid@live.com>
2025-12-12 20:19:46 +0400

    Bug fix

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,6 @@
 import createIntlPlugin from 'next-intl/plugin'
 
-const withIntl = createIntlPlugin('./src/intl')
+const withIntl = createIntlPlugin('./src/intl/index.ts')
 
 /** @type {import('next').NextConfig} */
 const config = {}
