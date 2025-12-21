8285442228d288a78dfd953712d519d3fa714117
Ali Zahid <ali.zahid@live.com>
2025-12-21 15:32:04 +0400

    Dependencies upgraded

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -1,13 +1,13 @@
 import 'expo-dev-client'
 import 'expo-router/entry'
-import '@formatjs/intl-getcanonicallocales/polyfill-force' // 1
-import '@formatjs/intl-locale/polyfill-force' // 2
-import '@formatjs/intl-pluralrules/polyfill-force' // 3
-import '@formatjs/intl-pluralrules/locale-data/en' // 4
-import '@formatjs/intl-numberformat/polyfill-force' // 5
-import '@formatjs/intl-numberformat/locale-data/en' // 6
-import '@formatjs/intl-relativetimeformat/polyfill-force' // 7
-import '@formatjs/intl-relativetimeformat/locale-data/en' // 8
+import '@formatjs/intl-getcanonicallocales/polyfill-force.js' // 1
+import '@formatjs/intl-locale/polyfill-force.js' // 2
+import '@formatjs/intl-pluralrules/polyfill-force.js' // 3
+import '@formatjs/intl-pluralrules/locale-data/en.js' // 4
+import '@formatjs/intl-numberformat/polyfill-force.js' // 5
+import '@formatjs/intl-numberformat/locale-data/en.js' // 6
+import '@formatjs/intl-relativetimeformat/polyfill-force.js' // 7
+import '@formatjs/intl-relativetimeformat/locale-data/en.js' // 8
 
 import '~/styles/uni'
 
