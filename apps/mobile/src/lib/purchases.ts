84a0c11a995b4a59f812378d1d9c54184486937a
Ali Zahid <ali.zahid@live.com>
2026-01-09 09:51:56 +0400

    Disable RevenueCat logging

--- apps/mobile/src/lib/purchases.ts
+++ apps/mobile/src/lib/purchases.ts
@@ -1,6 +1,8 @@
 // biome-ignore lint/style/noExportedImports: go away
 import purchases from 'react-native-purchases'
 
+purchases.setLogLevel(purchases.LOG_LEVEL.ERROR)
+
 purchases.configure({
   apiKey: process.env.EXPO_PUBLIC_REVENUE_CAT_KEY,
 })
