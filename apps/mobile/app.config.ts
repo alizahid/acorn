aaab76e63d1bb959454df0fff6c2a0556339b915
Ali Zahid <ali.zahid@live.com>
2026-02-12 12:04:27 +0500

    Version bumped

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -100,7 +100,7 @@ export default function getConfig(context: ConfigContext): ExpoConfig {
     },
     ios: {
       appleTeamId: process.env.APPLE_TEAM_ID,
-      buildNumber: '70',
+      buildNumber: '71',
       bundleIdentifier: 'blue.acorn',
       config: {
         usesNonExemptEncryption: false,
--- changelog.md
+++ changelog.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Upcoming
+## Build 71-72
 
 - added: settings tab always available
 - fixed: blur nsfw
