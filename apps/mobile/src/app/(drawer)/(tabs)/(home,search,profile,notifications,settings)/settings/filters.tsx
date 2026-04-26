bbe4544cbad07f125a7eeb3287f62422f958e244
Ali Zahid <ali.zahid@live.com>
2026-04-26 17:42:37 +0500

    Refactoring

--- apps/mobile/src/app/(drawer)/(tabs)/(home,search,profile,notifications,settings)/settings/filters.tsx
+++ apps/mobile/src/app/(drawer)/(tabs)/(home,search,profile,notifications,settings)/settings/filters.tsx
@@ -3,8 +3,7 @@ import { useFocusEffect, useNavigation } from 'expo-router'
 import { useCallback } from 'react'
 import { FormProvider, useFieldArray } from 'react-hook-form'
 import { FlatList, View } from 'react-native'
-import { ScrollView } from 'react-native-gesture-handler'
-import { KeyboardAwareScrollView } from 'react-native-keyboard-controller'
+import { KeyboardAvoidingView } from 'react-native-keyboard-controller'
 import { StyleSheet } from 'react-native-unistyles'
 import { useTranslations } from 'use-intl'
 import { z } from 'zod'
@@ -16,6 +15,7 @@ import { Text } from '~/components/common/text'
 import { FilterCard } from '~/components/filters/card'
 import { useFilters } from '~/hooks/filters'
 import { heights } from '~/lib/common'
+import { listProps } from '~/lib/list'
 
 const schema = z.object({
   filters: z.array(
@@ -63,78 +63,78 @@ export default function Screen() {
 
   return (
     <FormProvider {...form}>
-      <FlatList
-        contentContainerStyle={styles.content}
-        data={filters.fields}
-        ItemSeparatorComponent={() => <View style={styles.separator} />}
-        keyExtractor={(item) => item.key}
-        ListHeaderComponent={
-          <View style={styles.header}>
-            {[
-              {
-                label: t('rules.yes.1'),
-                type: 'yes',
-              },
-              {
-                label: t('rules.yes.2'),
-                type: 'yes',
-              },
-              {
-                label: t('rules.no.1'),
-                type: 'no',
-              },
-              {
-                label: t('rules.no.2'),
-                type: 'no',
-              },
-              {
-                label: t('rules.no.3'),
-                type: 'no',
-              },
-              {
-                label: t('rules.info.1'),
-                type: 'info',
-              },
-            ].map((rule) => (
-              <View key={rule.label} style={styles.headerItem}>
-                <Icon
-                  name={
-                    rule.type === 'yes'
-                      ? 'checkmark.circle.fill'
-                      : rule.type === 'no'
-                        ? 'xmark.circle.fill'
-                        : 'info.circle.fill'
-                  }
-                  uniProps={(theme) => ({
-                    size: theme.typography[2].lineHeight,
-                    tintColor:
+      <KeyboardAvoidingView
+        automaticOffset
+        behavior="padding"
+        keyboardVerticalOffset={16}
+        style={styles.main}
+      >
+        <FlatList
+          {...listProps}
+          contentContainerStyle={styles.content}
+          data={filters.fields}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          keyExtractor={(item) => item.key}
+          ListHeaderComponent={
+            <View style={styles.header}>
+              {[
+                {
+                  label: t('rules.yes.1'),
+                  type: 'yes',
+                },
+                {
+                  label: t('rules.yes.2'),
+                  type: 'yes',
+                },
+                {
+                  label: t('rules.no.1'),
+                  type: 'no',
+                },
+                {
+                  label: t('rules.no.2'),
+                  type: 'no',
+                },
+                {
+                  label: t('rules.no.3'),
+                  type: 'no',
+                },
+                {
+                  label: t('rules.info.1'),
+                  type: 'info',
+                },
+              ].map((rule) => (
+                <View key={rule.label} style={styles.headerItem}>
+                  <Icon
+                    name={
                       rule.type === 'yes'
-                        ? theme.colors.green.accent
+                        ? 'checkmark.circle.fill'
                         : rule.type === 'no'
-                          ? theme.colors.red.accent
-                          : theme.colors.blue.accent,
-                  })}
-                />
+                          ? 'xmark.circle.fill'
+                          : 'info.circle.fill'
+                    }
+                    uniProps={(theme) => ({
+                      size: theme.typography[2].lineHeight,
+                      tintColor:
+                        rule.type === 'yes'
+                          ? theme.colors.green.accent
+                          : rule.type === 'no'
+                            ? theme.colors.red.accent
+                            : theme.colors.blue.accent,
+                    })}
+                  />
 
-                <Text size="2" style={styles.rule}>
-                  {rule.label}
-                </Text>
-              </View>
-            ))}
-          </View>
-        }
-        renderItem={({ index }) => (
-          <FilterCard index={index} onRemove={filters.remove} />
-        )}
-        renderScrollComponent={(props) => (
-          <KeyboardAwareScrollView
-            {...props}
-            bottomOffset={16}
-            // @ts-expect-error
-            ScrollViewComponent={ScrollView}
-          />
-        )}
-      />
+                  <Text size="2" style={styles.rule}>
+                    {rule.label}
+                  </Text>
+                </View>
+              ))}
+            </View>
+          }
+          renderItem={({ index }) => (
+            <FilterCard index={index} onRemove={filters.remove} />
+          )}
+        />
+      </KeyboardAvoidingView>
 
       <FloatingButton
         color="red"
@@ -180,6 +180,9 @@ const styles = StyleSheet.create((theme) => ({
     alignItems: 'center',
     flexDirection: 'row',
     gap: theme.space[2],
+  },
+  main: {
+    flex: 1,
   },
   rule: {
     flex: 1,
