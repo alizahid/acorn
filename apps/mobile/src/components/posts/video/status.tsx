f9bbb65001b8b864d2e8a559c681563272947ef2
Ali Zahid <ali.zahid@live.com>
2026-05-26 05:22:39 +0500

    Bug fix

--- apps/mobile/src/components/posts/video/status.tsx
+++ apps/mobile/src/components/posts/video/status.tsx
@@ -2,11 +2,12 @@ import { useEventListener } from 'expo'
 import { type VideoPlayer } from 'expo-video'
 import { View } from 'react-native'
 import Animated, {
+  cancelAnimation,
   useAnimatedStyle,
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated'
-import { StyleSheet, useUnistyles } from 'react-native-unistyles'
+import { StyleSheet } from 'react-native-unistyles'
 
 type Props = {
   player: VideoPlayer
@@ -16,54 +17,70 @@ export function VideoStatus({ player }: Props) {
   const current = useSharedValue(0)
   const buffered = useSharedValue(0)
 
-  const { theme } = useUnistyles()
-
   useEventListener(player, 'timeUpdate', (event) => {
-    current.set(() =>
-      withTiming((event.currentTime / player.duration) * 100 || 0, {
-        duration: 1000 / 1000 / 60,
-      }),
-    )
+    const nextCurrent = (event.currentTime / player.duration) * 100
 
-    buffered.set(() =>
-      withTiming((event.bufferedPosition / player.duration) * 100 || 0, {
-        duration: 1000 / 1000 / 60,
-      }),
-    )
+    if (current.get() !== nextCurrent) {
+      cancelAnimation(current)
+
+      current.set(() =>
+        withTiming((event.currentTime / player.duration) * 100, {
+          duration: 1000 / 60,
+        }),
+      )
+    }
+
+    const nextBuffered = (event.bufferedPosition / player.duration) * 100
+
+    if (buffered.get() !== nextBuffered) {
+      cancelAnimation(buffered)
+
+      buffered.set(() =>
+        withTiming(nextBuffered, {
+          duration: 1000 / 60,
+        }),
+      )
+    }
   })
 
   const currentStyle = useAnimatedStyle(() => ({
-    backgroundColor: theme.colors.accent.accent,
     width: `${current.get()}%`,
   }))
 
   const bufferedStyle = useAnimatedStyle(() => ({
-    backgroundColor: theme.colors.accent.uiAlpha,
     width: `${buffered.get()}%`,
   }))
 
   return (
     <View style={styles.main}>
-      <Animated.View style={[styles.bar, bufferedStyle]} />
+      <Animated.View style={[styles.bar, styles.buffered, bufferedStyle]} />
 
-      <Animated.View style={[styles.bar, currentStyle]} />
+      <Animated.View style={[styles.bar, styles.current, currentStyle]} />
     </View>
   )
 }
 
 const styles = StyleSheet.create((theme) => ({
   bar: {
+    borderCurve: 'continuous',
+    borderRadius: theme.space[1],
     bottom: 0,
     height: theme.space[1],
     left: 0,
     position: 'absolute',
     right: 0,
   },
+  buffered: {
+    backgroundColor: theme.colors.accent.uiAlpha,
+  },
+  current: {
+    backgroundColor: theme.colors.accent.accent,
+  },
   main: {
     backgroundColor: theme.colors.gray.uiAlpha,
     bottom: 0,
-    left: 0,
+    left: -theme.space[1],
     position: 'absolute',
-    right: 0,
+    right: -theme.space[1],
   },
 }))
