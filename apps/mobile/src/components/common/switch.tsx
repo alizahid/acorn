89cc9bda27e9eed458a02abd085e31d5d510e9a0
Ali Zahid <ali.zahid@live.com>
2026-07-16 14:19:27 +0400

    Bug fix

--- apps/mobile/src/components/common/switch.tsx
+++ apps/mobile/src/components/common/switch.tsx
@@ -1,25 +1,34 @@
-import { Host, Toggle } from '@expo/ui/swift-ui'
-import { labelsHidden, tint } from '@expo/ui/swift-ui/modifiers'
+import {
+  Switch as Component,
+  type StyleProp,
+  View,
+  type ViewStyle,
+} from 'react-native'
 import { useUnistyles } from 'react-native-unistyles'
 
 type Props = {
   disabled?: boolean
   label: string
   onChange?: (value: boolean) => void
+  style?: StyleProp<ViewStyle>
   value?: boolean
 }
 
-export function Switch({ label, onChange, value }: Props) {
+export function Switch({ label, disabled, style, onChange, value }: Props) {
   const { theme } = useUnistyles()
 
   return (
-    <Host colorScheme={theme.variant} matchContents>
-      <Toggle
-        isOn={value}
-        label={label}
-        modifiers={[tint(theme.colors.accent.accent), labelsHidden()]}
-        onIsOnChange={onChange}
+    <View style={style}>
+      <Component
+        accessibilityLabel={label}
+        disabled={disabled}
+        onValueChange={onChange}
+        trackColor={{
+          false: theme.colors.gray.accent,
+          true: theme.colors.accent.accent,
+        }}
+        value={value}
       />
-    </Host>
+    </View>
   )
 }
